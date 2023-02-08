62c5f2bb3b6897095038f085ccb0845d4d75f171
rrd108 <rrd@webmania.cc>
2023-02-08 14:30:43 +0100

    pinia ts added

--- src/store.ts
+++ src/store.ts
@@ -1,8 +1,12 @@
 import { defineStore } from 'pinia'
 import User from './types/User'
 
+interface State {
+  user: User
+}
+
 export const useStore = defineStore('sadhana', {
-  state: () => ({
+  state: (): State => ({
     user: {} as User,
   }),
 })
