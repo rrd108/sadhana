eb22c1c233ffe308d694b03f6d19253cbccc2da5
rrd108 <rrd@webmania.cc>
2023-02-08 14:30:53 +0100

    header implemented

--- src/components/AppHeader.vue
+++ src/components/AppHeader.vue
@@ -1,11 +1,26 @@
-<script setup lang="ts"></script>
+<script setup lang="ts">
+  import { useStore } from '../store'
+  const store = useStore()
+</script>
 
 <template>
-  <header>header</header>
+  <header>
+    <img alt="Sadhana" src="@/assets/logo.png" />
+    {{ store.user.email.split('@')[0] }}
+  </header>
 </template>
 
 <style scoped>
   header {
+    height: 10vh;
+    display: grid;
+    grid-template-columns: 1fr 5fr;
+    align-items: center;
+    justify-items: center;
+    font-size: 2rem;
+    font-weight: bold;
+  }
+  img {
     height: 10vh;
   }
 </style>
