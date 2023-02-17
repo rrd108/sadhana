8f9a87fa3359b9b010ee26da610ccfb81648e218
rrd108 <rrd@webmania.cc>
2023-02-17 15:53:10 +0100

    file removed

--- src/composables/getDateData..spec.ts
+++
@@ -1,8 +0,0 @@
-import { toISO } from '@/composables/getDateData'
-
-describe('toISO', () => {
-  it('should find the tags', () => {
-    expect(toISO('2022. 04. 26.')).toBe('2022-04-26')
-    expect(toISO('2022/04/26')).toBe('2022-04-26')
-  })
-})
