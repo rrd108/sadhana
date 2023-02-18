2bca81b36149fffa727629e53bd8b05c976ffeb2
rrd108 <rrd@webmania.cc>
2023-02-18 10:28:11 +0100

    production started

--- api/config/cors.php
+++ api/config/cors.php
@@ -1,7 +1,7 @@
 <?php
 return [
     'Cors' => [
-        'AllowOrigin' => ['http://localhost:5173'],
+        'AllowOrigin' => ['http://localhost:5173', 'https://sadhana.1108.cc'],
         'AllowMethods' => ['GET', 'POST', 'PATCH', 'OPTIONS'],
         'AllowHeaders' => ['Token', 'Content-Type'],
         'MaxAge' => 300,
