6a29c4c199ba94d16ed26e0e9e58aa565cb2f4e9
rrd108 <rrd@webmania.cc>
2023-03-26 16:07:21 +0200

    minimum pwa added

--- vite.config.ts
+++ vite.config.ts
@@ -1,6 +1,7 @@
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 import path from 'path'
+import { VitePWA } from 'vite-plugin-pwa'
 
 // https://vitejs.dev/config/
 export default defineConfig({
@@ -9,5 +10,33 @@ export default defineConfig({
       '@': path.resolve(__dirname, './src'), // map '@' to './src'
     },
   },
-  plugins: [vue()],
+  plugins: [
+    vue(),
+    VitePWA({
+      registerType: 'autoUpdate',
+      devOptions: {
+        // TODO remove for production
+        enabled: true,
+      },
+      includeAssets: ['favicon.ico', 'apple-touch-icon.png', 'masked-icon.svg'],
+      manifest: {
+        name: 'Sadhana App',
+        short_name: 'Sadhana',
+        description: 'Sadhana',
+        theme_color: '#31003e',
+        icons: [
+          {
+            src: 'android-chrome-192x192.png',
+            sizes: '192x192',
+            type: 'image/png',
+          },
+          {
+            src: 'android-chrome-512x512.png',
+            sizes: '512x512',
+            type: 'image/png',
+          },
+        ],
+      },
+    }),
+  ],
 })
