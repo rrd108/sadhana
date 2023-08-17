f5cebb016f94d1fa1c76c973122618025f477a17
rrd108 <rrd@webmania.cc>
2023-08-17 16:01:48 +0200

    use dynamic import for better chunking

--- src/components/Welcome.vue
+++ src/components/Welcome.vue
@@ -2,9 +2,10 @@
   import { useRoute } from 'vue-router'
   import { useStore } from '../store'
   import Login from '@/pages/Login.vue'
-  import Register from '@/pages/Register.vue'
-  import ForgotPass from '@/pages/ForgotPass.vue'
-  import PassReset from '@/pages/PassReset.vue'
+
+  const Register = () => import('@/pages/Register.vue')
+  const ForgotPass = () => import('@/pages/ForgotPass.vue')
+  const PassReset = () => import('@/pages/PassReset.vue')
 
   const route = useRoute()
   const store = useStore()
@@ -12,14 +13,7 @@
 
 <template>
   <div>
-    <Login
-      v-if="
-        !store.user.id &&
-        !['Register', 'ForgotPass', 'PassReset'].includes(
-          route.name?.toString() || ''
-        )
-      "
-    />
+    <Login v-if="!store.user.id && !['Register', 'ForgotPass', 'PassReset'].includes(route.name?.toString() || '')" />
     <Register v-if="route.name == 'Register'" />
     <ForgotPass v-if="route.name == 'ForgotPass'" />
     <PassReset v-if="route.name == 'PassReset'" />
