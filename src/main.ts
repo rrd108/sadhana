a28ee0e8e947c5f7c0bb5daa1cf5f89774e89418
thomas <git@mlndks.nl>
2023-09-15 14:55:42 +0200

    add en-US to fallback locales
    This prevents the state where the locale is changed but has not finished loading the locale file.
    Before this state would show placholders. now it shows one of the fallback locales if one of them is loaded

--- src/main.ts
+++ src/main.ts
@@ -35,7 +35,7 @@ library.add(
 
 const i18n = setupI18n({
   legacy: false, // you must set `false`, to use Composition API
-  fallbackLocale: "hu",
+  fallbackLocale: ["hu", "en-US"],
 });
 
 const pinia = createPinia();
